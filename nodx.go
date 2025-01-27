77c71e25a6e666df11b3d6ed043ff8347bbaf2d2
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-27 18:17:54 +0000

    Improve NodX function documentation and comments

--- nodx.go
+++ nodx.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Node is the interface that wraps the basic Render methods used in the
-// text, attribute, and element nodes.
+// different NodX node types.
 type Node interface {
 	// Render writes the node HTML to the writer.
 	Render(w io.Writer) error
@@ -18,75 +18,57 @@ type Node interface {
 	RenderBytes() ([]byte, error)
 }
 
-// Group creates a new group of nodes.
-//
-// This helper function allows for grouping multiple nodes into a single node without
-// adding extra HTML elements.
-//
-// The resulting node renders its child nodes directly without any wrapping tag.
+// Group combines multiple nodes into a single node without wrapping them in any HTML tag.
+// It renders the child nodes directly.
 func Group(nodes ...Node) Node {
 	return newNodeGroup(nodes...)
 }
 
-// El creates a new nodx.Node representing an HTML element.
-//
-// Useful for creating HTML elements not included in this library.
+// El creates a new Node representing an HTML element with the given name.
 func El(name string, children ...Node) Node {
 	return newNodeElement(false, name, children...)
 }
 
-// ElVoid creates a new nodx.Node representing an HTML void element.
-//
-// Useful for creating HTML elements not included in this library.
+// ElVoid creates a new Node representing an HTML void element with the given name.
+// Void elements are self-closing, such as <img> or <input>.
 func ElVoid(name string, children ...Node) Node {
 	return newNodeElement(true, name, children...)
 }
 
-// Attr creates a new nodx.Node representing an HTML attribute.
-//
-// Useful for creating HTML attributes not included in this library.
+// Attr creates a new Node representing an HTML attribute with a name and value.
+// The value is HTML-escaped to prevent XSS attacks.
 func Attr(name string, value string) Node {
 	return newNodeAttribute(name, value)
 }
 
-// Text creates a new nodx.Node representing an HTML text node.
-// The value is escaped to ensure it is safe to use in HTML
-// preventing XSS attacks.
+// Text creates a new Node representing an escaped HTML text node.
+// The value is HTML-escaped to prevent XSS attacks.
 func Text(value string) Node {
 	return newNodeText(EscapeHTML(value))
 }
 
-// Text creates a new nodx.Node representing an HTML text node.
-// The value is escaped to ensure it is safe to use in HTML
-// preventing XSS attacks.
-//
-// The value is formatted using the fmt.Sprintf function.
+// Textf creates a new Node representing an escaped HTML text node.
+// The value is formatted with fmt.Sprintf and then HTML-escaped to prevent XSS attacks.
 func Textf(format string, a ...any) Node {
 	return Text(fmt.Sprintf(format, a...))
 }
 
-// Raw creates a new nodx.Node representing an HTML raw text node.
-// The value is not escaped, so it is the responsibility of the caller to
-// ensure that the value is safe to use in HTML.
-//
-// Useful when you need to render raw HTML such as <script> tags.
+// Raw creates a new Node representing a raw HTML text node.
+// The value is not escaped, so the caller must ensure the content is safe.
+// Useful for rendering raw HTML, like <script> or <style> tags.
 func Raw(value string) Node {
 	return newNodeText(value)
 }
 
-// Raw creates a new nodx.Node representing an HTML raw text node.
-// The value is not escaped, so it is the responsibility of the caller to
-// ensure that the value is safe to use in HTML.
-//
-// Useful when you need to render raw HTML such as <script> tags.
-//
-// The value is formatted using the fmt.Sprintf function.
+// Rawf creates a new Node representing a raw HTML text node.
+// The value is formatted with fmt.Sprintf and not escaped, so ensure its safety.
+// Useful for rendering raw HTML, like <script> or <style> tags.
 func Rawf(format string, a ...any) Node {
 	return Raw(fmt.Sprintf(format, a...))
 }
 
-// If renders the node if the provided condition is true, otherwise
-// it renders nothing.
+// If renders a Node based on the provided boolean condition.
+// If the condition is true, the node is rendered; otherwise, it renders nothing.
 func If(condition bool, node Node) Node {
 	if condition {
 		return node
@@ -94,8 +76,8 @@ func If(condition bool, node Node) Node {
 	return nil
 }
 
-// IfFunc executes the provided function and renders its result if the
-// provided condition is true, otherwise it renders nothing.
+// IfFunc executes and renders the result of a function based on the provided boolean condition.
+// If the condition is true, the function is executed and rendered; otherwise, nothing is executed nor rendered.
 func IfFunc(condition bool, function func() Node) Node {
 	if condition {
 		return function()
@@ -103,8 +85,8 @@ func IfFunc(condition bool, function func() Node) Node {
 	return nil
 }
 
-// Map accepts a slice of any type and a function that returns a Node
-// and returns a group of the nodes returned by the function.
+// Map transforms a slice of any type into a group of nodes by applying a function to each element.
+// Returns a single Node that contains all the resulting nodes.
 func Map[T any](slice []T, function func(T) Node) Node {
 	nodes := make([]Node, len(slice))
 	for i, item := range slice {
