905ad771ac9f96f5e048b3bee66424834e89d0d4
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-27 16:44:48 +0000

    Add Textf and Rawf formatting functions for text nodes

--- nodx.go
+++ nodx.go
@@ -1,6 +1,7 @@
 package nodx
 
 import (
+	"fmt"
 	"io"
 )
 
@@ -55,6 +56,15 @@ func Text(value string) Node {
 	return newNodeText(EscapeHTML(value))
 }
 
+// Text creates a new nodx.Node representing an HTML text node.
+// The value is escaped to ensure it is safe to use in HTML
+// preventing XSS attacks.
+//
+// The value is formatted using the fmt.Sprintf function.
+func Textf(format string, a ...any) Node {
+	return Text(fmt.Sprintf(format, a...))
+}
+
 // Raw creates a new nodx.Node representing an HTML raw text node.
 // The value is not escaped, so it is the responsibility of the caller to
 // ensure that the value is safe to use in HTML.
@@ -62,4 +72,15 @@ func Text(value string) Node {
 // Useful when you need to render raw HTML such as <script> tags.
 func Raw(value string) Node {
 	return newNodeText(value)
+}
+
+// Raw creates a new nodx.Node representing an HTML raw text node.
+// The value is not escaped, so it is the responsibility of the caller to
+// ensure that the value is safe to use in HTML.
+//
+// Useful when you need to render raw HTML such as <script> tags.
+//
+// The value is formatted using the fmt.Sprintf function.
+func Rawf(format string, a ...any) Node {
+	return Raw(fmt.Sprintf(format, a...))
 }
