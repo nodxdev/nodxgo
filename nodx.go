d1a00caab1ae7dafcc80f6e9562553be0dbb2064
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-27 14:45:34 +0000

    Add documentation comments to Node interface methods

--- nodx.go
+++ nodx.go
@@ -7,7 +7,12 @@ import (
 // Node is the interface that wraps the basic Render methods used in the
 // text, attribute, and element nodes.
 type Node interface {
+	// Render writes the node HTML to the writer.
 	Render(w io.Writer) error
+
+	// RenderString returns the node HTML as a string.
 	RenderString() (string, error)
+
+	// RenderBytes returns the node HTML as a byte slice.
 	RenderBytes() ([]byte, error)
 }
