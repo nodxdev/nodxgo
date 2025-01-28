6b82fce72eed394d3746ea23dec3777c528d3df2
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 18:53:11 +0000

    Add example test for IfFunc

---
+++ example_iffunc_test.go
@@ -0,0 +1,18 @@
+package nodx
+
+import "fmt"
+
+func ExampleIfFunc() {
+	condition := 3 > 2
+
+	node := Div(
+		Class("container"),
+		IfFunc(condition, func() Node {
+			return Text("Condition is true")
+		}),
+	)
+
+	fmt.Println(node)
+	// Output:
+	// <div class="container">Condition is true</div>
+}
