ba6de4755e649483f26d5f34894513664857f912
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 23:23:38 +0000

    Move ElVoid example test into example_el_test.go

--- example_el_test.go
+++ example_el_test.go
@@ -11,3 +11,13 @@ func ExampleEl() {
 	// Output:
 	// <my-custom-element>Hello, World!</my-custom-element>
 }
+
+func ExampleElVoid() {
+	node := ElVoid("my-custom-element",
+		Class("container"),
+	)
+
+	fmt.Println(node)
+	// Output:
+	// <my-custom-element class="container">
+}
