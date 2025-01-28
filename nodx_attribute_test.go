4fc58d0620081640f45924dbcc5a70c783929fc2
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 18:01:55 +0000

    Refactor NodeAttribute tests to use assert.Render helper

--- nodx_attribute_test.go
+++ nodx_attribute_test.go
@@ -1,7 +1,6 @@
 package nodx
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/nodxdev/nodxgo/internal/assert"
@@ -12,87 +11,39 @@ func TestNodeAttribute(t *testing.T) {
 		key := "class"
 		value := "container"
 		expected := `class="container"`
-
 		node := newNodeAttribute(key, value)
-		buf := new(bytes.Buffer)
-		err := node.Render(buf)
-		assert.NoError(t, err)
-		assert.Equal(t, expected, buf.String())
-	})
-
-	t.Run("Attribute render string", func(t *testing.T) {
-		key := "class"
-		value := "container"
-		expected := `class="container"`
-
-		node := newNodeAttribute(key, value)
-		got, err := node.RenderString()
-		assert.NoError(t, err)
-		assert.Equal(t, expected, got)
-	})
-
-	t.Run("Attribute stringer interface", func(t *testing.T) {
-		key := "class"
-		value := "container"
-		expected := `class="container"`
-
-		node := newNodeAttribute(key, value)
-		got := node.String()
-		assert.Equal(t, expected, got)
-	})
-
-	t.Run("Attribute render bytes", func(t *testing.T) {
-		key := "class"
-		value := "container"
-		expected := `class="container"`
-
-		node := newNodeAttribute(key, value)
-		got, err := node.RenderBytes()
-		assert.NoError(t, err)
-		assert.Equal(t, expected, string(got))
+		assert.Render(t, expected, node)
 	})
 
 	t.Run("Empty value attribute render", func(t *testing.T) {
 		key := "class"
 		value := ""
 		expected := `class=""`
-
 		node := newNodeAttribute(key, value)
-		got, err := node.RenderString()
-		assert.NoError(t, err)
-		assert.Equal(t, expected, got)
+		assert.Render(t, expected, node)
 	})
 
 	t.Run("Empty key attribute render", func(t *testing.T) {
 		key := ""
 		value := "container"
 		expected := ""
-
 		node := newNodeAttribute(key, value)
-		got, err := node.RenderString()
-		assert.NoError(t, err)
-		assert.Equal(t, expected, got)
+		assert.Render(t, expected, node)
 	})
 
 	t.Run("Empty key and value attribute render", func(t *testing.T) {
 		key := ""
 		value := ""
 		expected := ""
-
 		node := newNodeAttribute(key, value)
-		got, err := node.RenderString()
-		assert.NoError(t, err)
-		assert.Equal(t, expected, got)
+		assert.Render(t, expected, node)
 	})
 
 	t.Run("Double quote in value attribute render", func(t *testing.T) {
 		key := "class"
 		value := `container"container'container`
 		expected := `class="container&quot;container&#39;container"`
-
 		node := newNodeAttribute(key, value)
-		got, err := node.RenderString()
-		assert.NoError(t, err)
-		assert.Equal(t, expected, got)
+		assert.Render(t, expected, node)
 	})
 }
