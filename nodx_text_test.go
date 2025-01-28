3e831ceaeee7da2fe7db50fda571a9678f73b0eb
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 17:46:17 +0000

    Simplify nodeText test cases using Render utility

--- nodx_text_test.go
+++ nodx_text_test.go
@@ -1,7 +1,6 @@
 package nodx
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/nodxdev/nodxgo/internal/assert"
@@ -9,54 +8,20 @@ import (
 
 func TestNodeText(t *testing.T) {
 	t.Run("Text render", func(t *testing.T) {
-		text := "Hello, World!"
-		node := newNodeText(text)
-		buf := &strings.Builder{}
-
-		err := node.Render(buf)
-		assert.NoError(t, err)
-		assert.Equal(t, text, buf.String())
-	})
-
-	t.Run("Text render string", func(t *testing.T) {
-		text := "Hello, World!"
-		node := newNodeText(text)
-
-		str, err := node.RenderString()
-		assert.NoError(t, err)
-		assert.Equal(t, text, str)
-	})
-
-	t.Run("Text render bytes", func(t *testing.T) {
-		text := "Hello, World!"
-		node := newNodeText(text)
-
-		bytes, err := node.RenderBytes()
-		assert.NoError(t, err)
-		assert.Equal(t, text, string(bytes))
+		expected := "Hello, World!"
+		node := newNodeText(expected)
+		assert.Render(t, expected, node)
 	})
 
 	t.Run("Empty text render", func(t *testing.T) {
+		expected := ""
 		node := newNodeText("")
-		buf := &strings.Builder{}
-
-		err := node.Render(buf)
-		assert.NoError(t, err)
-		assert.Equal(t, "", buf.String())
+		assert.Render(t, expected, node)
 	})
 
 	t.Run("Not initialized nodeText", func(t *testing.T) {
+		expected := ""
 		node := nodeText{}
-		buf := &strings.Builder{}
-
-		err := node.Render(buf)
-		assert.NoError(t, err)
-		assert.Equal(t, "", buf.String())
-	})
-
-	t.Run("Text Stringer interface", func(t *testing.T) {
-		text := "Hello, World!"
-		node := newNodeText(text)
-		assert.Equal(t, text, node.String())
+		assert.Render(t, expected, node)
 	})
 }
