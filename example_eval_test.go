7d49bfa5fe494a767f6412f4af62cdcbf19c6282
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 18:47:48 +0000

    Add example test for Eval function

---
+++ example_eval_test.go
@@ -0,0 +1,23 @@
+package nodx
+
+import "fmt"
+
+func ExampleEval() {
+	condition := 3 > 2
+
+	node := Group(
+		Div(
+			Class("container"),
+			Eval(func() Node {
+				if condition {
+					return Text("Condition is true")
+				}
+				return Text("Condition is false")
+			}),
+		),
+	)
+
+	fmt.Println(node)
+	// Output:
+	// <div class="container">Condition is true</div>
+}
