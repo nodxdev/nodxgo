cca92ffe6e0092d35c53d8713fee6fcc65330f7f
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 18:37:35 +0000

    Add RenderNoSpaces utility for whitespace-insensitive node rendering tests

--- internal/assert/render.go
+++ internal/assert/render.go
@@ -37,3 +37,36 @@ func Render(t *testing.T, expected string, node Node) {
 	gotStr = node.String()
 	Equal(t, expected, gotStr)
 }
+
+// RenderNoSpaces is the same as Render, but it removes all the white
+// spaces and similar characters from the expected and the output to
+// make the test more readable without the need to remove spaces
+// manually when writing the test.
+func RenderNoSpaces(t *testing.T, expected string, node Node) {
+	t.Helper()
+
+	removeSpaces := func(s string) string {
+		for _, char := range []string{"\n", "\t", "\r", " "} {
+			s = strings.ReplaceAll(s, char, "")
+		}
+		return s
+	}
+
+	expected = removeSpaces(expected)
+
+	buf := &strings.Builder{}
+	err := node.Render(buf)
+	NoError(t, err)
+	Equal(t, expected, removeSpaces(buf.String()))
+
+	gotStr, err := node.RenderString()
+	NoError(t, err)
+	Equal(t, expected, removeSpaces(gotStr))
+
+	gotBytes, err := node.RenderBytes()
+	NoError(t, err)
+	Equal(t, expected, removeSpaces(string(gotBytes)))
+
+	gotStr = node.String()
+	Equal(t, expected, removeSpaces(gotStr))
+}
