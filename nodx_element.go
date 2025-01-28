837d9403ed1790bb6bf583a07c4d4c67556784eb
Luis Eduardo <eduardo.devop@gmail.com>
2025-01-28 17:19:50 +0000

    Refactor nodeElement rendering with categorizeChildren method
    Introduce a new categorizeChildren function to separate element and attribute nodes during rendering. This improves the rendering logic by:
    - Handling nil and group nodes
    - Separating elements and attributes before rendering
    - Simplifying the Render method's child processing logic

--- nodx_element.go
+++ nodx_element.go
@@ -26,25 +26,54 @@ func newNodeElement(isVoid bool, name string, children ...Node) nodeElement {
 	}
 }
 
+// categorizeChildren categorizes the children into elements and attributes.
+//
+//   - The nil nodes are ignored.
+//   - Groups are expanded so that the nodes in the group become children of the
+//     element.
+//
+// It returns two slices, the first are elements and the second are attributes.
+func categorizeChildren(children []Node) ([]Node, []Node) {
+	elements := []Node{}
+	attributes := []Node{}
+
+	for _, child := range children {
+		if child == nil {
+			continue
+		}
+		if group, ok := child.(nodeGroup); ok {
+			els, attrs := categorizeChildren(group)
+			elements = append(elements, els...)
+			attributes = append(attributes, attrs...)
+			continue
+		}
+		if child.IsElement() {
+			elements = append(elements, child)
+		}
+		if child.IsAttribute() {
+			attributes = append(attributes, child)
+		}
+	}
+
+	return elements, attributes
+}
+
 func (ne nodeElement) Render(w io.Writer) error {
 	if ne.name == "" {
 		return nil
 	}
+
+	els, attrs := categorizeChildren(ne.children)
 
 	_, err := fmt.Fprintf(w, "<%s", ne.name)
 	if err != nil {
 		return err
 	}
 
-	for _, child := range ne.children {
-		if child == nil {
-			continue
-		}
-		if child.IsAttribute() {
-			err = child.Render(w)
-			if err != nil {
-				return err
-			}
+	for _, attr := range attrs {
+		err = attr.Render(w)
+		if err != nil {
+			return err
 		}
 	}
 
@@ -54,15 +83,10 @@ func (ne nodeElement) Render(w io.Writer) error {
 	}
 
 	if !ne.isVoid {
-		for _, child := range ne.children {
-			if child == nil {
-				continue
-			}
-			if child.IsElement() {
-				err = child.Render(w)
-				if err != nil {
-					return err
-				}
+		for _, el := range els {
+			err = el.Render(w)
+			if err != nil {
+				return err
 			}
 		}
 
